520085a781631fd659fabc3e37bcc79e286507fc
Saikhnaaaaa <114391679+Saikhnaaaaa@users.noreply.github.com>
2023-10-21 13:55:33 +0800

    todo
    todo

---
+++ Todo.js
@@ -0,0 +1,164 @@
+import React, { useState } from "react"; 
+import { 
+	View, 
+	Text, 
+	TextInput, 
+	TouchableOpacity, 
+	FlatList, 
+	StyleSheet, 
+} from "react-native"; 
+
+const Todo = () => { 
+	const [task, setTask] = useState(""); 
+	const [tasks, setTasks] = useState([]); 
+	const [editIndex, setEditIndex] = useState(-1); 
+
+	const handleAddTask = () => { 
+		if (task) { 
+			if (editIndex !== -1) { 
+				const update = [...tasks]; 
+				update[editIndex] = task; 
+				setTasks(update); 
+				setEditIndex(-1); 
+			} else { 
+				setTasks([...tasks, task]); 
+			} 
+			setTask(""); 
+		} 
+	}; 
+
+	const handleEditTask = (index) => { 
+		const taskEdit = tasks[index]; 
+		setTask(taskEdit); 
+		setEditIndex(index); 
+	}; 
+
+	const handleDeleteTask = (index) => { 
+		const update = [...tasks]; 
+		update.splice(index, 1); 
+		setTasks(update); 
+	}; 
+
+	const todoItem = ({ item, index }) => ( 
+		<View style={styles.task}> 
+			<Text 
+				style={styles.itemList}>{item}</Text> 
+			<View 
+				style={styles.taskButtons}> 
+				<TouchableOpacity 
+					onPress={() => handleEditTask(index)}> 
+					<Text 
+						style={styles.editButton}>Edit</Text> 
+				</TouchableOpacity> 
+				<TouchableOpacity 
+					onPress={() => handleDeleteTask(index)}> 
+					<Text 
+						style={styles.deleteButton}>Delete</Text> 
+				</TouchableOpacity> 
+			</View> 
+		</View> 
+	); 
+
+	return ( 
+		<View style={styles.container}> 
+			<Text style={styles.title}>Todo List!</Text> 
+			<View style={styles.yoko}>
+			<TextInput 
+				style={styles.input} 
+				placeholder="Write task"
+				value={task} 
+				onChangeText={(text) => setTask(text)} 
+			/> 
+			<TouchableOpacity 
+				style={styles.addButton} 
+				onPress={handleAddTask}> 
+				<Text style={styles.addButtonText}> 
+					{editIndex !== -1 ? "Update" : "Add"} 
+				</Text> 
+			</TouchableOpacity> 
+			</View>
+			
+			<FlatList 
+				data={tasks} 
+				renderItem={todoItem} 
+				keyExtractor={(item, index) => index.toString()} 
+			/> 
+		</View> 
+	); 
+}; 
+
+const styles = StyleSheet.create({ 
+	container: { 
+		flex: 1, 
+		padding: 40, 
+		marginTop: 40, 
+	}, 
+	yoko: {
+		width: "300",
+		height: "200",
+		// backgroundColor: "white",
+		flexDirection:"row",
+		
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	title: { 
+		fontSize: 24, 
+		fontWeight: "bold", 
+		marginBottom: 20, 
+	}, 
+	heading: { 
+		fontSize: 30, 
+		fontWeight: "bold", 
+		marginBottom: 7, 
+		color: "green", 
+	}, 
+	input: { 
+		width: 260,
+		borderWidth: 3, 
+		borderColor: "#ccc", 
+		padding: 10, 
+		marginBottom: 10, 
+		borderRadius: 10, 
+		fontSize: 18, 
+	}, 
+	addButton: { 
+		backgroundColor: "grey", 
+		marginLeft: 30,
+		padding: 10, 
+		borderRadius: 5, 
+		marginBottom: 10, 
+	}, 
+	addButtonText: { 
+		color: "white", 
+		fontWeight: "bold", 
+		textAlign: "center", 
+		fontSize: 18, 
+	}, 
+	task: { 
+		flexDirection: "row", 
+		justifyContent: "space-between", 
+		alignItems: "center", 
+		marginBottom: 15, 
+		fontSize: 18, 
+	}, 
+	itemList: { 
+		fontSize: 19, 
+	}, 
+	taskButtons: { 
+		flexDirection: "row", 
+	}, 
+	editButton: { 
+		marginRight: 10, 
+		color: "black", 
+		fontWeight: "bold", 
+		fontSize: 18, 
+	}, 
+	deleteButton: { 
+		color: "red", 
+		fontWeight: "bold", 
+		fontSize: 18, 
+	}, 
+}); 
+
+export default Todo;
